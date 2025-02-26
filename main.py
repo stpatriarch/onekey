c60c8c337271e007c522d68d1c0d8efa1f93f839
Narek Chinaryan <chinaryannarek@gmail.com>
2025-02-26 18:10:34 +0400

    modified password generation

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-import hashlib, base64, re
+import hashlib
+import base64
 
 
-def get_user_input(label, requered=False, domain=False):
+def get_user_input(label, requered=False):
+        
         value = input(f'{label}') or None
         while requered and not value:
             value = input(f'{label}') or None
-        if domain and re.match('.', value):
-            value = value.split('.')[0]
-        return value.lower()    
+        return value    
 
-def hash_it(phrase):     
-     hashsum = hashlib.sha256(str.encode(phrase)).digest()
-     finalsum = base64.b64encode(hashsum).decode('ascii')
-     return finalsum[:-1]
-
+def get_hash_sum(secret_phrase):
+     hash_sum = hashlib.sha256(str.encode(secret_phrase)).digest()
+     return base64.b64encode(hash_sum).decode('ascii')
 def main():
-    secret_phrase = input('Please enter your unical phrase: ')
+    secret_phrase = input('Please enter your unical secret phrase: ')
     while not len(secret_phrase) == 64:
-        secret_phrase = input('Please enter your unical phrase: ')
+        secret_phrase = input('Please enter your unical secret phrase: ')
         print('Your unical secret phrase must be a have 64 characters')
     
-    username = get_user_input('enter your username: ', requered=True)
-    domain = get_user_input('please enter account domain: ', requered=True, domain=True)
-
-    unihash = hash_it(f'{username + domain}')
-    final_hash = hash_it(f'{secret_phrase + unihash}')
-    print(f'Your password for {username} in {domain} is:  {final_hash}')
+    # username = get_user_input('enter your username: ', requered=True)
+    domain = get_user_input('please enter account domain: ', requered=True)
+    
+    # unihash = hash_it(f'{username + domain}')
+    final_hash = get_hash_sum(f'{secret_phrase + domain}')
+    password = f'Your password for {domain} is:  {final_hash[16:]}'
+    print(password)
+    return 0
 
 if __name__ == '__main__':
     main()
