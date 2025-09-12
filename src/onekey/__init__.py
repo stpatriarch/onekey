fad2e5d10f69c66a8e6dc098d19f8a6996745da2
stpatriarch <chinaryannarek@gmail.com>
2025-09-13 01:46:56 +0400

    separate a hash services from onekeycore

--- src/onekey/__init__.py
+++ src/onekey/__init__.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("onekey")
+except PackageNotFoundError:
+    __version__ = "\"_\""
+
+
+__title__ = "onekey"
+__description__ = "Password meneger based on sha256 hash."
+__url__ = "https://github.com/stpatriarch/onekey"
+
+__author__ = "stpatriarch"
+__author_email__ = "chinaryannarek@gmail.com"
+__license__ = "MIT"
+
--- src/onekey/core/core.py
+++ src/onekey/core/core.py
@@ -1,36 +1,53 @@
 #!/usr/bin/env python3
 
 # Այստեղ իրականացված են գործիքի բոլոր բազզային ֆունկցիոնալ հնարավորությունները։
+
 import hashlib
 import base64
 from getpass import getpass
 
+class Hasher:
 
-class OneKeyCore:
-     
-    def __init__(self, unikey=None, domain=None):
+    def __init__(self):
 
-        self.domain_for_show = domain if domain else None
-        self.req_len  = 64
-        self.ukey = self.final_hash_sum(unikey) if unikey else self.get_key()
-        self.domain = self.final_hash_sum(domain.lower()) if domain else self.get_domain()
-        
+        self.hexhash = lambda x: hashlib.sha256(str.encode(x)).hexdigest()
+
+
     def get_hash_sum(func):
         def wrapper(self):     
             return hashlib.sha256(str.encode(func(self))).hexdigest() 
-        return wrapper   
+        return wrapper      
+
+    @property
+    def final_hash_sum(self):
+        hash_sum = hashlib.sha256(str.encode(self.ukey + self.domain)).digest()
+        return base64.b64encode(hash_sum).decode('ascii')
+
+
+class OneKeyCore(Hasher):
+     
+    def __init__(self, unikey=None, domain=None):
+
+        super().__init__()
+        self.domain_for_show = domain if domain else None
+        self.ukey = self.hexhash(unikey) if unikey else self.get_key()
+        self.domain = self.hexhash(domain.lower()) if domain else self.get_domain()
+        
+ 
     
-    @get_hash_sum
+    @Hasher.get_hash_sum
     def get_key(self):
-         
+
+        required_len = 64
+        
         secret_phrase = getpass('\U0001F510 Please enter your unical secret key: ')
 
-        while not len(secret_phrase) == self.req_len:
+        while not len(secret_phrase) == required_len:
               secret_phrase = getpass('\U0001F514 Please enter valid, 64 character unical secret key: ')
 
         return secret_phrase
 
-    @get_hash_sum 
+    @Hasher.get_hash_sum 
     def get_domain(self):
         
         domain = input('\U0001F3E2 please enter account domain: ') or None
@@ -41,9 +58,10 @@ class OneKeyCore:
         return domain.lower()   
 
     def close(self):
+        
+        is_tuple = isinstance(self.import_error_handle, tuple)
 
-        if isinstance(self.import_error_handle, tuple):
-
+        if is_tuple:
             print(f'\U00002705 Your password for {self.domain_for_show.upper()} copied to clipboard.')
         
         else:
@@ -53,18 +71,12 @@ class OneKeyCore:
     def import_error_handle(self):
         try:
             import  pyperclip
-            return pyperclip.copy(self.final_hash_sum()), []
+            return pyperclip.copy(self.final_hash_sum), []
         
         except ImportError:
-            return self.final_hash_sum()    
+            return self.final_hash_sum   
 
 
-    def final_hash_sum(self, arg=None):
-        
-        if arg:
-            return hashlib.sha256(str.encode(arg)).hexdigest()
-        hash_sum = hashlib.sha256(str.encode(self.ukey + self.domain)).digest()
-        return base64.b64encode(hash_sum).decode('ascii')
 
     
    
